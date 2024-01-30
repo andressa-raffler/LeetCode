eda37b16a13819cbc379e260c90abc03fef30af0
andressa-raffler <andressa.raffler@gmail.com>
2024-01-30 15:01:27 -0300

    added gitIgnore

---
+++ LeetCodeExcercices/src/test/java/com/raffler/leetcodeexcercices/LeetCodeExcercicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raffler.leetcodeexcercices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeExcercicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
